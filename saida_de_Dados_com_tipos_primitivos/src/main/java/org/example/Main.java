e7788d050c7c9f2770db78f2690833e490de542d
lukas <lukasrocha0402@gmail.com>
2024-03-14 12:04:05 -0300

    Adicionando dados com tipos primitivos

---
+++ saida_de_Dados_com_tipos_primitivos/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int nota = 9;
+        System.out.printf("A nota de lucas foi: " + nota);
+
+    }
+}
