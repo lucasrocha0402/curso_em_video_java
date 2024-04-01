e887f391bf5e2c31615985dd6568e000163b5b32
lukas <lukasrocha0402@gmail.com>
2024-04-01 20:17:29 -0300

    Adicionando operadores unarios

---
+++ OperadoresUnarios.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+       // int numero = 5;
+        // colocando o ++(incremento) antes da soma ele vai somar o valor antigo antes, se colocar depois da varivael vai ser pos a soma
+        // += somar e atribuir. -= subtrair e atribuir. *= multiplicar e atribuir. /= dividir e atribuir. %= resto e atribuir.
+        // int valor = 4 + numero-- ;
+        // System.out.println(valor);
+        // System.out.println(numero);
+        int x = 4;
+        x += 2;
+        System.out.println(x);
+    }
+}
